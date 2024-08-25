e59b18ee3a1c00978797239eff63fb7a749efdec
yuchen <yc.yang413@gmail.com>
2024-08-24 22:51:15 -0400

    certified collision rate

--- train_verify/verify_preimage.py
+++ train_verify/verify_preimage.py
@@ -1,7 +1,7 @@
 import torch
 import os
+# os.environ['AUTOLIRPA_DEBUG'] = '1'
 
-os.environ['AUTOLIRPA_DEBUG'] = '1'
 from auto_LiRPA import BoundedModule, BoundedTensor, PerturbationLpNorm
 import torch.nn as nn
 from tqdm import tqdm
@@ -39,7 +39,7 @@ def get_opts():
     parser.add_argument("--verbose", help="Print intermediate statistics", action="store_true")
     parser.add_argument("--in_dim", default=64, type=int)
     parser.add_argument('--epsilon',
-                        default=8. / 255, type=float)
+                        default=1. / 255, type=float)
     parser.add_argument('--model',
                         default='../train_verify/mnist_pdq_ep1/ckpt_best.pth', type=str)
     return parser.parse_args()
@@ -85,7 +85,6 @@ class ModifiedModel(nn.Module):
         self.remove_c.bias.data = torch.zeros((input_dim))
 
     def forward(self, x):
-        #TODO: Does this correct?
         x = x.view(-1, self.channel * self.in_dim * self.in_dim)
         x = self.apply_c(x)
         x = self.remove_c(x)
@@ -97,7 +96,6 @@ class ModifiedModel(nn.Module):
 
 
 def get_preimage(model_ori, val_dataloader, eps, dummy_input, threshold=90):
-    # with torch.no_grad():
     apply_output_constraints_to = ['BoundMatMul', 'BoundInput']
     model = BoundedModule(model_ori, dummy_input, bound_opts={
         "conv_mode": "patches",
@@ -111,9 +109,13 @@ def get_preimage(model_ori, val_dataloader, eps, dummy_input, threshold=90):
             'iteration': 1000,
         }
     })
+    x_L_list = []
+    x_U_list = []
     for i, data in enumerate(tqdm(val_dataloader)):
+        # if i >=2:
+        #     break
         try:
-            x, _ = data
+            x, y = data
             x = x.cuda()
             model.eval()
             y_clean = model(x)
@@ -134,49 +136,56 @@ def get_preimage(model_ori, val_dataloader, eps, dummy_input, threshold=90):
             #fixme: the correct one should be:
             model.thresholds = torch.tensor([(y_clean + threshold), -(y_clean - threshold)],
                                             dtype=torch.float32)  # this is -d
+            model.compute_bounds(x=(bounded_x,), method='CROWN-Optimized')
+            tightened_ptb = model['/0'].perturbation
+            x_L = tightened_ptb.x_L
+            x_U = tightened_ptb.x_U
+            x_L_list.append(x_L.squeeze(0).detach().cpu())
+            x_U_list.append(x_U.squeeze(0).detach().cpu())
 
-            # c = model.constraints
-            # model.init_alpha(
-            #     (bounded_x,), share_alphas=False, c=c, bound_upper=False)
-            lb, ub = model.compute_bounds(x=(bounded_x,), method='CROWN-Optimized')
-
-            # First use absolut then add up. If the pdg lb/ub bounds is within the verification lb/ub.
-
-            print('clean input:', y_clean)
-            print('LB', lb)
-            print('UB', ub)
-
-            # print(model['/0'].lower)
-            # print(model['/0'].upper)
-            # tightened_ptb = model['/0'].perturbation
-            # print('xL:',tightened_ptb.x_L)
-            # print('xU:',tightened_ptb.x_U)
-
+            # print('xL:',x_L)
+            # print('xU:',x_U)
             # ori_image = x.squeeze().cpu().detach().numpy()
             # lb_image = tightened_ptb.x_L.squeeze().cpu().detach().numpy()
             # ub_image = tightened_ptb.x_U.squeeze().cpu().detach().numpy()
             # combined_image = np.concatenate((ori_image, lb_image, ub_image), axis=1)
             # plt.imsave('e8_combined_image.png', combined_image, cmap='gray')  # cmap='gray' for grayscale
-            break
+            # break
 
         except RuntimeError as e:
             if 'CUDA out of memory' in str(e):
                 print(f"Error: CUDA out of memory.")
-                break  # Break out of the loop on CUDA memory error
+                torch.cuda.empty_cache()
+                continue
             else:
                 raise  # Re-raise the exception if it's not a memory error
 
+    torch.save(torch.stack(x_L_list), 'saved_preimages/ep0_X_L.pt')
+    torch.save(torch.stack(x_U_list), 'saved_preimages/ep0_X_U.pt')
+
+    count_collision = 0
+    total_samples = 10000
+    X_L = torch.load('saved_preimages/ep0_X_L.pt')
+    X_U = torch.load('saved_preimages/ep0_X_U.pt')
+    cannot_reach_preimage = total_samples-len(X_L)
+    for i in range(len(X_L)-1):
+        overlaps_lower = X_L[i] <= X_U[i+1:]  # Compare with all following elements
+        overlaps_upper = X_U[i] >= X_L[i+1:]
+        total_overlap = overlaps_lower.all() & overlaps_upper.all()
+        count_collision += total_overlap.sum().item()
+    print(f"Verified collision rate: {(count_collision+cannot_reach_preimage)/total_samples}")
 
 def main():
     opts = get_opts()
     opts.in_dim = 28
     channel = 1
     opts.val_data = '/home/yuchen/code/verified_phash/train_verify/data/mnist/mnist_test.csv'
-    opts.model = '/home/yuchen/code/verified_phash/train_verify/saved_models/mnist_pdq_ep1/ckpt_best.pth'
-    # opts.model = '/home/yuchen/code/verified_phash/Normal-Training/mnist-pdq.pt.pt'
+    # opts.model = '/home/yuchen/code/verified_phash/train_verify/saved_models/mnist_pdq_ep1/ckpt_best.pth'
+    opts.model = '/home/yuchen/code/verified_phash/train_verify/saved_models/mnist_pdq_ep0/last_epoch_state_dict.pth'
     val_data = ImageToHashAugmented_PDQ(opts.val_data, opts.data_dir, resize=opts.in_dim, num_augmented=0)
     # model = resnet_v5(num_classes=144, input_dim=opts.in_dim)
     val_dataloader = DataLoader(val_data, batch_size=opts.batch_size, shuffle=True, pin_memory=True)
+    val_dataloader_all = DataLoader(val_data, batch_size=10000, shuffle=True, pin_memory=True)
     model = resnet(in_ch=1, in_dim=opts.in_dim)
     model_weights = torch.load(opts.model)
     model.load_state_dict(model_weights)
